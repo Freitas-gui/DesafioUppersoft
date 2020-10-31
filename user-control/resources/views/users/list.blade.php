d738f55fcd1b7858640cdb3699a3f3ee6c2436f7
Guilheme Freitas <freitas-gui19@hotmail.com>
2020-10-31 17:18:43 -0300

    table of show list

--- user-control/resources/views/users/list.blade.php
+++ user-control/resources/views/users/list.blade.php
@@ -15,10 +15,34 @@
                     @endif
 
                     <h1>List of client users</h1>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Editar</th>
+                                <th scope="col">Deletar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
                     @foreach( $clientUsers as $u )
-                        <p> {{ $u->name }} </p>
-                        <p> {{ $u->email }} </p>
+
+                            <tr>
+                                <th scope="row">{{ $u->id }}</th>
+                                <td>{{ $u->name }}</td>
+                                <td>{{ $u->email }}</td>
+                                <td>
+                                    <a class="btn btn-info">Editar</a>
+                                </td>
+                                <td>
+                                    <a class="btn btn-danger">Deletar</a>
+                                </td>
+                            </tr>
+
                     @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
